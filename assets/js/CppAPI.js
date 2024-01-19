a70a8513bfd7b72184b07f9aa08baced77062332
Marco Prescher <m.prescher@gantner-instruments.com>
2024-01-19 16:21:46 +0100

    added delete functionality in frontend

--- assets/js/CppAPI.js
+++ assets/js/CppAPI.js
@@ -25,10 +25,51 @@ class CppAPI {
         }
 
         try {
-            SaveTask(task);
-            return true;
+            let statusCode = SaveTask(task);
+            if (statusCode) {
+                return true;
+            }
         } catch (e) {
             console.warn("[CppAPI] ultralight binding error - function 'SaveTask'");
+        }
+
+        return false;
+    }
+
+    static deleteTaskById(taskId) {
+        if (taskId == null) {
+            return false;
+        }
+
+        taskId = parseInt(taskId);
+        if (isNaN(taskId)) {
+            return false;
+        }
+
+        try {
+            let statusCode = DeleteTaskById(taskId);
+            if (statusCode) {
+                return true;
+            }
+        } catch (e) {
+            console.warn("[CppAPI] ultralight binding error - function 'DeleteTaskById'");
+        }
+
+        return false;
+    }
+
+    static updateTask(task) {
+        if (task == null) {
+            return false;
+        }
+
+        try {
+            let statusCode = UpdateTask(task);
+            if (statusCode) {
+                return true;
+            }
+        } catch (e) {
+            console.warn("[CppAPI] ultralight binding error - function 'UpdateTask'");
         }
 
         return false;
